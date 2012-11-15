9bf9e143bf78f840d1419e147913b8657edfb92e
Shockah <lolshockah@gmail.com>
2012-11-15 22:56:22 +0100

    Lol, I broke Java. Fixed EFontAlign

--- src/pl/shockah/easyslick/EFontAlign.java
+++ src/pl/shockah/easyslick/EFontAlign.java
@@ -3,19 +3,19 @@ package pl.shockah.easyslick;
 public enum EFontAlign {
 	TopLeft(0,0), TopCenter(1,0), TopRight(2,0), MiddleLeft(0,1), MiddleCenter(1,1), MiddleRight(2,1), BottomLeft(0,2), BottomCenter(1,2), BottomRight(2,2);
 	
-	public static final EFontAlign[] array = new EFontAlign[9];
-	public static final EFontAlign[][] pos = new EFontAlign[3][3];
+	public static final EFontAlign[] array;
+	public static final EFontAlign[][] pos;
+	
+	static {
+		array = new EFontAlign[]{TopLeft,TopCenter,TopRight,MiddleLeft,MiddleCenter,MiddleRight,BottomLeft,BottomCenter,BottomRight};
+		pos = new EFontAlign[][]{{TopLeft,MiddleLeft,BottomLeft},{TopCenter,MiddleCenter,BottomCenter},{TopRight,MiddleRight,BottomRight}};
+	}
 	
 	public final int x, y;
 	
 	private EFontAlign(int x, int y) {
 		this.x = x;
 		this.y = y;
-		init();
-	}
-	private void init() {
-		pos[x][y] = this;
-		array[ordinal()] = this;
 	}
 	
 	public EFontAlign mirror(boolean h, boolean v) {
